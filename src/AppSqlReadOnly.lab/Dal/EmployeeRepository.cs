9ae8fac72b1b4f1715a4f833251beba386bd07c9
DatabaseDropper <47637776+DatabaseDropper@users.noreply.github.com>
2020-12-12 10:16:16 +0100

    init

---
+++ src/AppSqlReadOnly.lab/Dal/EmployeeRepository.cs
@@ -0,0 +1,51 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppSqlReadOnly.lab.Dal
+{
+    public class EmployeeRepository : Repository, IEmployeesRepository
+    {
+        public EmployeeRepository(IDbConnection connectnion, ISqlQueries sqlQueries) : base (connectnion, sqlQueries, nameof(EmployeeRepository))
+        {
+        }
+
+        public async Task<IEnumerable<EmployeeDao>> GetAllAsync()
+        {
+            return await Connection.QueryAsync<EmployeeDao>(GetSqlQuery());
+        }
+
+        public async Task<EmployeeDao> GetAsync(int id)
+        {
+            return await Connection.QueryFirstOrDefaultAsync<EmployeeDao>(GetSqlQuery(), new { Param = id });
+        }
+
+        public async Task<IEnumerable<EmployeeDao>> GetByCityAsync(string city)
+        {
+            return await Connection.QueryAsync<EmployeeDao>(GetSqlQuery(), new { Param = city });
+        }
+
+        // Tests
+
+        public async Task<EmployeeDao> Test_GetAsync(int id)
+        {
+            // kompromis pomiędzy liczbą plików z SQLami dla danych warunków, a trzymaniu części SQL w kodzie 
+            var query = string.Format(GetWhereSQL(), $"WHERE {nameof(EmployeeDao.Id)} = @Param");
+            return await Connection.QueryFirstOrDefaultAsync<EmployeeDao>(query, new { Param = id });
+        }
+
+        public async Task<IEnumerable<EmployeeDao>> Test_GetByCityAsync(string city)
+        {
+            var query = string.Format(GetWhereSQL(), $"WHERE {nameof(EmployeeDao.City)} = @Param");
+            return await Connection.QueryAsync<EmployeeDao>(query, new { Param = city });
+        }
+
+        private string GetWhereSQL()
+        {
+            return GetSqlQuery();
+        }
+    }
+}
---
+++ src/AppSqlReadOnly.lab/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AppSqlReadOnly.lab.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
